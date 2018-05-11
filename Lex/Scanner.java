3e123e13bd527db8852df280e1d0c1fb72ca2026
Dario Facchinetti <dario.fad@gmail.com>
2018-05-11 22:04:42 +0200

    First lexer version.

--- Lex/Scanner.java
+++ Lex/Scanner.java
@@ -1,4 +1,3 @@
-import java.io.FileReader;
 import java.lang.*;
 
 import org.antlr.v4.runtime.*;
@@ -9,11 +8,21 @@ public class Scanner {
 
     public static void main(String[] args) {
         Token tk;
+        int i = 0;
         try {
-            //TODO
-            //CharStream inputStream= new CharStream("p.ciao");
-            //EmmetLexer lexer = new EmmetLexer(inputStream);
 
+            //creazione input stream
+            org.antlr.v4.runtime.ANTLRInputStream in_str = new org.antlr.v4.runtime.ANTLRInputStream("p>h.class");
+            //istanziazione del lexer generato da antlr
+            EmmetLexer lexer = new EmmetLexer(in_str);
+            //lettura del primo token
+            tk = lexer.nextToken();
+            while (tk.getType() != Token.EOF) {
+                i++;
+                System.out.println(i + "->[" + tk.getLine() + "," + tk.getCharPositionInLine() + "]\t" +
+                        " TokenType:" + tk.getType() + "\t:" + tk.getText());
+                tk = lexer.nextToken();
+            }
         } catch (Exception e) {
             System.out.println("Lexer aborted: " + e.getMessage());
         }
