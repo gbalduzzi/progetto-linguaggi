6e51b12ad2b9f41144ca89f0754d312f6fc25986
Dario Facchinetti <dario.fad@gmail.com>
2018-05-11 22:25:01 +0200

    First parser version.

--- Lex/Scanner.java
+++ Lex/Scanner.java
@@ -9,14 +9,17 @@ public class Scanner {
     public static void main(String[] args) {
         Token tk;
         int i = 0;
-        try {
+        //try {
+        //creazione input stream
+        org.antlr.v4.runtime.ANTLRInputStream in_str = new org.antlr.v4.runtime.ANTLRInputStream("p>h.class");
+        //istanziazione del lexer generato da antlr
+        EmmetLexer lexer = new EmmetLexer(in_str);
 
-            //creazione input stream
-            org.antlr.v4.runtime.ANTLRInputStream in_str = new org.antlr.v4.runtime.ANTLRInputStream("p>h.class");
-            //istanziazione del lexer generato da antlr
-            EmmetLexer lexer = new EmmetLexer(in_str);
-            //lettura del primo token
-            tk = lexer.nextToken();
+        /**
+         * La versione 1 del lexer, non si fa altro che iterare (lo lascio per velocizzare)
+         */
+        //lettura del primo token
+            /*tk = lexer.nextToken();
             while (tk.getType() != Token.EOF) {
                 i++;
                 System.out.println(i + "->[" + tk.getLine() + "," + tk.getCharPositionInLine() + "]\t" +
@@ -25,6 +28,19 @@ public class Scanner {
             }
         } catch (Exception e) {
             System.out.println("Lexer aborted: " + e.getMessage());
+        }*/
+
+        /**
+         * Ora sviluppo della versione 2, uno stream di token che viene inviato al parser(non stampiamo nulla a console)
+         * In questo modo, potremo aggiungere la logica estendendo il listener
+         */
+        //creazione del token stream
+        org.antlr.v4.runtime.CommonTokenStream tk_stream = new org.antlr.v4.runtime.CommonTokenStream(lexer, Token.DEFAULT_CHANNEL);
+        //creazione del parser
+        EmmetParser parser = new EmmetParser(tk_stream);
+        //chiamata al contesto (alla regola di partenza)
+        parser.s();
         }
-    }
+
+
 }
